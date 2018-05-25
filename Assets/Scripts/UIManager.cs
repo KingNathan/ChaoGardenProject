39e94e58e720bd7038cae5a0e35d6ba5d7df1ba5
kingnathan <nathandupuis14@gmail.com>
2018-05-25 14:54:41 -0400

    Deuxieme commit - Debut de drag and drop; Shop active/inactive;

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     public GameObject m_ShopPanel;
+    public TextMeshProUGUI m_ShopButtonText;
 
     public void Update()
     {
-        if(Input.GetKey(KeyCode.S))
+        if(Input.GetKeyDown(KeyCode.S))
         {
             OnMenuClick();
         }
@@ -17,5 +19,9 @@ public class UIManager : MonoBehaviour
     public void OnMenuClick()
     {
         m_ShopPanel.SetActive(!m_ShopPanel.activeSelf);
+        if (m_ShopPanel.activeSelf)
+            m_ShopButtonText.text = "Close";
+        else
+            m_ShopButtonText.text = "Shop";
     }
 }
